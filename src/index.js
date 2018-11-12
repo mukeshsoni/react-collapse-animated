69c9aa1bd09bebce064e7d2a9423c6bccb40264f
Mukesh Soni <mukeshsoni@gmail.com>
2018-11-12 19:55:16 +0530

    bug fix for height adjustment when content height changes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,67 @@
-import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
 
 export default class Collapse extends PureComponent {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       height: 0
-    }
-    this.containerRef = null
+    };
+    this.containerRef = null;
   }
 
   componentDidMount() {
-    if (this.props.open) {
-      this.setState({
-        height: this.containerRef ? this.containerRef.scrollHeight : 0
-      })
+    const { transitionDuration } = this.props;
+
+    if (this.props.open && this.containerRef) {
+      this.setState(
+        {
+          height: this.containerRef.clientHeight
+        },
+        () => {
+          // Once the animation is complete, set the height to auto
+          // This ensures that if the content height changes, the parent height
+          // adjust automatically
+          setTimeout(() => {
+            this.setState({ height: "auto" });
+          }, parseInt(transitionDuration, 10) * 1000);
+        }
+      );
     }
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps.open !== this.props.open) {
-      this.setState({
-        height: this.containerRef ? this.containerRef.scrollHeight : 0
-      })
+    if (nextProps.open !== this.props.open && this.containerRef) {
+      if (nextProps.open) {
+        this.setState(
+          {
+            height: this.containerRef.scrollHeight
+          },
+          () => {
+            setTimeout(() => {
+              this.setState({ height: "auto" });
+            }, 200);
+          }
+        );
+      } else {
+        // this is a case where the component is going from open to close state
+        // When it's open, we set the height of container to 'auto'
+        // But browers don't know how to transition from 'auto' to zero value
+        // So we first set the height to the current height of content
+        // And then set it to zero
+        this.setState({ height: this.containerRef.clientHeight }, () => {
+          // Directly calling setState with height 0 does not work
+          // Even though the render method is called with the changed height and
+          // height 0 twice. --\O/--
+          // weirdly, i couldn't get it to work consistently with a setTimeout of 0 or even
+          // 1. It would work sometimes and not at other times. So i said, ok, i will
+          // wait for 20ms before it animates
+          setTimeout(() => {
+            this.setState({ height: 0 });
+          }, 20);
+        });
+      }
     }
   }
 
@@ -35,15 +73,15 @@ export default class Collapse extends PureComponent {
       style,
       transitionDuration,
       transitionType
-    } = this.props
-    const { height } = this.state
+    } = this.props;
+    const { height } = this.state;
 
     return (
       <div
         style={{
-          overflow: 'hidden',
+          overflow: "hidden",
           transition: `height ${transitionDuration} ${transitionType}`,
-          height: open ? height : 0,
+          height,
           ...style
         }}
         ref={node => (this.containerRef = node)}
@@ -52,7 +90,7 @@ export default class Collapse extends PureComponent {
       >
         {children}
       </div>
-    )
+    );
   }
 }
 
@@ -66,10 +104,9 @@ Collapse.propTypes = {
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
   ]).isRequired
-}
+};
 
 Collapse.defaultProps = {
-  transitionDuration: '0.3s',
-  transitionType: 'ease-in',
-  maxHeight: 10000
-}
+  transitionDuration: "0.3s",
+  transitionType: "ease-in"
+};
